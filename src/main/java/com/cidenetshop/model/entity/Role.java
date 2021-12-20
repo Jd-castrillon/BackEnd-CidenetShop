526e02e8cbb092ad49170e2c59ac783398872ab0
Habib De León Baños <hdeleon@cidenet.com.co>
2021-12-20 14:25:55 -0500

    wip

--- src/main/java/com/cidenetshop/model/entity/Role.java
+++ src/main/java/com/cidenetshop/model/entity/Role.java
@@ -5,14 +5,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "roles")
 public class Role {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private static final String ID_SEQUENCE_GENERATOR_NAME = "roles_id_sequence_generator";
+    private static final String ID_SEQUENCE_NAME = "roles_id_sequence";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = ID_SEQUENCE_GENERATOR_NAME)
+    @SequenceGenerator(name = ID_SEQUENCE_GENERATOR_NAME, sequenceName = ID_SEQUENCE_NAME, allocationSize = 1)
 	@Column(name = "id")
 	private Integer id;
 
